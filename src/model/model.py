1668148bf13927fc0a8d19b6e37ecea29b99b474
Nicolás Iglesias <nfiglesias@gmail.com>
2023-08-26 11:11:02 -0300

    refactor(model): float mode

--- src/model/model.py
+++ src/model/model.py
@@ -155,6 +155,11 @@ else:
                 u = u.contiguous()
                 k = k.contiguous()
                 v = v.contiguous()
+            elif '16' in os.environ['RWKV_FLOAT_MODE']:
+                w = -torch.exp(w.half().contiguous())
+                u = u.half().contiguous()
+                k = k.half().contiguous()
+                v = v.half().contiguous()
             else:
                 w = -torch.exp(w.float().contiguous())
                 u = u.float().contiguous()
